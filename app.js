9f6a56fd383cff26f5174757774c40209f67aeff
Maadeha shaikh <maadeham@gmail.com>
2024-08-21 19:11:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const userModel = require('./models/user')
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+app.set("view engine","ejs");
+app.use(express.json()); 
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname , 'public')));
+app.use(cookieParser());
+
+app.get("/",(req,res) =>{
+  res.render("index");
+})
+
+app.post('/create', (req,res)=>{
+  let {username , email , password , age} = req.body;
+    bcrypt.genSalt(10,(err,salt) => {
+    bcrypt.hash(password , async (err,hash) => {
+      let createdUser =  await userModel.create({
+        username ,
+        email,
+        password : hash,
+        age
+      })
+      let token = jwt.sign({email} , "secret");
+      res.cookie("token",token)
+      res.send(createdUser); 
+    })
+  })
+})
+
+app.get("/login", (req,res) => {
+   res.render('login')
+})
+
+app.post("/login",async (req,res) => {
+  let user = await userModel.findOne({email : req.body.email})
+  if(!user) return res.send("something went wrong ");
+
+  bcrypt.compare(req.body.password , user.password , function(err,result){
+    // console.log(result) undefined printing 
+    if(result) {
+      let token = jwt.sign({email : user.email} , "secret");
+      res.cookie("token",token)
+      res.send("Yes you can now login !")
+    }
+    else res.send("something went wrong")
+  })
+})
+
+app.get("/logout",(req,res)=> {
+  res.cookie("token","");
+  res.redirect("/");
+})
+
+
+app.listen(3000);
